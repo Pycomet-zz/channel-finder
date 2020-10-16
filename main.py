536624f6e5df15fd983fa9bc0b7970a5fdca6518
Codefred <alfredemmanuelinyang@gmail.com>
2020-10-17 00:09:41 +0100

    testing

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from telethon.sync import TelegramClient, events
+from telethon.tl.types import InputPeerChannel
 import asyncio
-import os
+from decouple import config
 
 # Defining the need variables
-api_id = os.getenv('API_ID') #Input your api_id here
-api_hash = os.getenv('API_HASH') #Input your api_hash here
+api_id = config('API_ID') #Input your api_id here
+api_hash = config('API_HASH') #Input your api_hash here
 
 loop = asyncio.new_event_loop()
 client = TelegramClient('runner', api_id, api_hash, loop=loop)
 client.start()
 
-channel = client.get_entity('https://t.me/DayTradingAlerts')
+channel = client.get_input_entity('https://t.me/techguided')
 
-import pdb; pdb.set_trace()
+chat = InputPeerChannel(channel_id=channel.channel_id, access_hash=channel.access_hash) # Input channel id and access_hash
+
+# import pdb; pdb.set_trace()
+channel = client.get_entity(1270515419)
 
 # Handler to receive the channel message
-@client.on(events.NewMessage(chats=channel, incoming=True)
+@client.on(events.NewMessage(incoming=True))
 async def handler(event):
-    await event.reply(f'{event.stringify()}')
+    await client.send_message('codefred', f'{event.stringify()}')
 
 
 client.add_event_handler(handler)
 
 # Run endlessly 
-client.run_until_disconnected()
+client.run_until_disconnected() 
